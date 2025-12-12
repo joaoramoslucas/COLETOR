a83cad0cdf29a124679af4bce810e7e2f41c012d
João Lucas <117485663+joaoramoslucas@users.noreply.github.com>
2025-12-12 14:53:11 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.post('/salvar', (req, res) => {
+    const texto = req.body.texto;
+    const data = new Date().toLocaleString();
+    const linha = `[${data}] ${texto}\n`;
+
+    fs.appendFile('storage.txt', linha, (err) => {
+        if (err) {
+            res.status(500).send('Nao foi possivel salvar');
+        } else {
+            res.send("salvo com sucesso");
+        }
+    });
+});
+
+app.get('/ler', (req, res) => {
+    if(!fs.existsSync('storage.txt')) {
+        return res.send('Nenhuma anotacao ainda.');
+    }
+
+    fs.readFile('storage.txt', 'utf-8', (err, data) => {
+        if(err) {
+            res.status(500).send("Erro ao ler")
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando! link de acesso : http://localhost:3000")
+})
